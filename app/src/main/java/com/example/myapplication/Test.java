f6c63e3642afa12c9cc491f18646f107f1e9ecd0
jiangzhengquan <694320253@qq.com>
2019-12-08 15:02:40 +0800

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/Test.java
@@ -0,0 +1,169 @@
+package com.example.myapplication;
+
+public class Test {
+
+
+    /**
+     * id : 123
+     * url : http://img.donever.com/aa/bb.jpg
+     * width : 500
+     * height : 500
+     * likeCount : 1
+     * description : 嘿嘿
+     * time : 1234567890
+     * replyCount : 0
+     * floorCount : 0
+     * likeUserCount : 5
+     * age : 14
+     * name : jack
+     * school : beijing
+     * type : 1
+     * sax : boy
+     * userid : 1223
+     */
+
+    private int id;
+    private String url;
+    private int width;
+    private int height;
+    private int likeCount;
+    private String description;
+    private int time;
+    private int replyCount;
+    private int floorCount;
+    private int likeUserCount;
+    private int age;
+    private String name;
+    private String school;
+    private int type;
+    private String sax;
+    private int userid;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getLikeCount() {
+        return likeCount;
+    }
+
+    public void setLikeCount(int likeCount) {
+        this.likeCount = likeCount;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getTime() {
+        return time;
+    }
+
+    public void setTime(int time) {
+        this.time = time;
+    }
+
+    public int getReplyCount() {
+        return replyCount;
+    }
+
+    public void setReplyCount(int replyCount) {
+        this.replyCount = replyCount;
+    }
+
+    public int getFloorCount() {
+        return floorCount;
+    }
+
+    public void setFloorCount(int floorCount) {
+        this.floorCount = floorCount;
+    }
+
+    public int getLikeUserCount() {
+        return likeUserCount;
+    }
+
+    public void setLikeUserCount(int likeUserCount) {
+        this.likeUserCount = likeUserCount;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSchool() {
+        return school;
+    }
+
+    public void setSchool(String school) {
+        this.school = school;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public String getSax() {
+        return sax;
+    }
+
+    public void setSax(String sax) {
+        this.sax = sax;
+    }
+
+    public int getUserid() {
+        return userid;
+    }
+
+    public void setUserid(int userid) {
+        this.userid = userid;
+    }
+}
